e2bdee89856b1b6f5059b1f913d000c7694cd32e
MAGES0451 <lucas.ribeiro.cruz12@gmail.com>
2024-06-27 11:50:33 -0300

    a

--- Components/footer.js
+++ Components/footer.js
@@ -6,12 +6,42 @@ class footer extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-    <footer>
-    
+  <footer>
+
+    <div class="container">
+
+      <div class="item item-1"></div>
+
+      <div class="item item-2"><a href="index.html">
+          <h3>Inicio</h3>
+        </a>
+      </div>
 
 
-    </footer>
+      <div class="item item-3">
+        <h3>Area do Coordenador</h3>
+        <ul>
+          <a href="PagCoord.html"><li>Status</li></a>
+        </ul>
+      </div>
 
+
+      <div class="item item-4">
+        <h3>Area do Professor</h3>
+        <ul>
+          <a href="status.html">
+            <li>Status</li>
+          </a>
+          <l1><a href="justificativa.html">
+              <li>Justificativa de Faltas</l1></a>
+          <l1><a href="reposicao.html">
+              <li>Plano de Reposição</l1></a>
+        </ul>
+      </div>
+
+    </div>
+
+  </footer>
 
     `;
 
